1657c86f303d86007e82f6f50d6c206ce2bb2c0e
Daganbold <ganbolddandar@gmail.com>
2023-03-15 00:17:31 -0700

    connect css

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,11 +2,11 @@ const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'f7581ab109mshb2448b70913ae59p140944jsn7252faffa6f0',
-		'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+		'X-RapidAPI-Host': 'uncovered-treasure-v1.p.rapidapi.com'
 	}
 };
 
-fetch('https://coinranking1.p.rapidapi.com/search-suggestions?referenceCurrencyUuid=yhjMzLPhuIDl', options)
+fetch('https://uncovered-treasure-v1.p.rapidapi.com/topics', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
