21a746a3102b85ce400ce4dd810fd70199dd743f
Dandar Ganbold <ganbolddandar@gmail.com>
2023-03-20 18:41:25 -0700

    create header baseline

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,26 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'f7581ab109mshb2448b70913ae59p140944jsn7252faffa6f0',
-		'X-RapidAPI-Host': 'uncovered-treasure-v1.p.rapidapi.com'
-	}
-};
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '34a30b9c25mshcf53a973bb4ac8cp1302c3jsn02d818ce1d62',
+// 		'X-RapidAPI-Host': 'local-business-data.p.rapidapi.com'
+// 	}
+// };
 
-fetch('https://uncovered-treasure-v1.p.rapidapi.com/topics', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+// fetch('https://local-business-data.p.rapidapi.com/search?query=Bars%20in%20San%20Francisco&limit=20&lat=37.359428&lng=-121.925337&zoom=13&language=en&region=us', options)
+// 	.then(response => response.json())
+//     .then(function(data){
+// 		console.log(data)
+// 	getData(data.data);
+	
+//    });
+
+//    function getData (arrayList){
+	
+// 	 //create a div with id name something in html then
+// 	 var container = document.getElementById("something");
+// 	 for(let i=0; i<  arrayList.length; i++){
+// 	   var contentName = arrayList[i].name;
+// 	   var contentRating = arrayList[i].rating;
+// 	   container.innerHTML += `<li> ${contentName} = ${contentRating}</li>`
+// 	 }
+//    }
