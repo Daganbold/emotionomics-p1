96adc69e6c7a5fc7f0a90f1729b8244bf5df46ba
Sonam Sherpa <sherpa.sjs@gmail.com>
2023-03-21 19:51:53 -0700

    get coin value from api

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,51 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '34a30b9c25mshcf53a973bb4ac8cp1302c3jsn02d818ce1d62',
-		'X-RapidAPI-Host': 'local-business-data.p.rapidapi.com'
-	}
+//variable
+var selectEl = document.getElementById("coinsDropDown");
+var coinEl = document.getElementById("coinChange");
+
+var options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "34a30b9c25mshcf53a973bb4ac8cp1302c3jsn02d818ce1d62",
+    "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+  },
 };
 
-fetch('https://local-business-data.p.rapidapi.com/search?query=Bars%20in%20San%20Francisco&limit=20&lat=37.359428&lng=-121.925337&zoom=13&language=en&region=us', options)
-	.then(response => response.json())
-    .then(function(data){
-		console.log(data)
-	getData(data.data);
-	
-   });
+fetch(
+  "https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=100&offset=0",
+  options
+)
+  .then((response) => {
+    return response.json();
+  })
+  .then((objectData) => {
+    console.log(objectData)
+    getData(objectData.data.coins);
+  });
 
-   function getData (arrayList){
-	
-	 //create a div with id name something in html then
-	 var container = document.getElementById("something");
-	 for(let i=0; i<  arrayList.length; i++){
-	   var contentName = arrayList[i].name;
-	   var contentRating = arrayList[i].rating;
-	   container.innerHTML += `<li> ${contentName} = ${contentRating}</li>`
-	 }
-   }
+function getData(arrayList) {
+  for (let i = 0; i < arrayList.length; i++) {
+    let option = document.createElement("option");
+    option.setAttribute("value", arrayList[i].change);
+
+    let optionText = document.createTextNode(arrayList[i].name);
+    option.appendChild(optionText);
+    selectEl.appendChild(option);
+  }
+}
+
+// event listener
+if (selectEl) {
+  selectEl.addEventListener("change", (event) => {
+    let coinChange = event.target.value;
+    console.log(coinChange);
+    if (coinChange > 1) {
+      coinEl.textContent = ` Your coin change is ${coinChange} now. Drink Beer`;
+      console.log(" Drink Beer....");
+    } else {
+      coinEl.textContent = `Your coin change is ${coinChange} now. Drink Water`;
+      console.log("drink water");
+    }
+  });
+}
+
+
